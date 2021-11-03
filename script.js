92f079a433c60d61ab79ce433e0e9a71218cce38
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-11-02 17:46:28 -0700

    update gameboard with marker upon user click

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-const game = (() => {
-  gameboard: [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
+const game = ((doc) => {
+  const gameboard = [null, null, null, null, null, null, null, null, null];
+  const _addedMarterToGameboard = true;
+  const _cannotAddMarkerToGameboard = false;
+
+  const hasDocumentObj = () => {
+    return !!doc && querySelector in doc;
+  };
+
+  // const isPositionEmpty = (position) => {
+  //   return gameboard[position-1] === null
+  // };
+
+  const canAddToPosition = (position) => {
+    return gameboard[position - 1] === null;
+  };
+
+  const addMarkerAtPosition = (marker, position) => {
+    if (!hasDocumentObj || !canAddToPosition(position)) {
+      return _cannotAddMarkerToGameboard;
+    }
+
+    gameboard[position - 1] = marker;
+    return _addedMarterToGameboard;
+  };
+
+  const updateBoard = () => {
+    if (!hasDocumentObj) return;
+
+    gameboard.forEach((marker, position) => {
+      doc.querySelector(`#position-${position + 1}`).innerHTML =
+        marker === null ? "" : marker;
+    });
+  };
 
   return {
     gameboard,
+    addMarkerAtPosition,
+    updateBoard,
   };
-})();
+})(document);
 
 const Player = (marker) => {
   const playerName = `Player ${marker}`;
-  const marker = marker;
+  const playerMarker = marker;
   return {
-    marker,
+    playerMarker,
     playerName,
   };
 };
+
+const boardPositions = document.querySelectorAll(".file");
+boardPositions.forEach((boardPosition) => {
+  boardPosition.addEventListener("click", (e) => {
+    const positionNumber = parseInt(e.target.id.slice(-1));
+    console.log(positionNumber);
+    game.addMarkerAtPosition("P", positionNumber);
+    // console.log(game.gameboard);
+    game.updateBoard();
+  });
+});
+
+console.log(game.gameboard);
